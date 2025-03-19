43395092d26d61f4db860734dc4bfbc6977ca82a
kau18920 <kau18920@sheridancollege.ca>
2025-03-19 17:46:07 -0400

    Update Player.java
    Added Functionality to Player.java

--- src/warcardgroup7/Player.java
+++ src/warcardgroup7/Player.java
@@ -4,13 +4,32 @@
  */
 package warcardgroup7;
 
-import java.util.List;
+import java.util.*;
+
 
 /**
  *
  * @author khali_jwhe8us
  */
 public class Player {
-    String name;
-    List<Card> hand;
+
+     public Queue<Card> hand = new LinkedList<>();
+    
+    public Player(String username, String password) {
+        super(username, password);
+    }
+    
+    public void addCard(Card card) {
+        hand.offer(card); 
+    }
+    public Card drawCard() {
+        return hand.poll(); 
+    }
+    public int getHandSize() {
+        return hand.size();
+    }
+    public boolean hasCards() {
+        return !hand.isEmpty(); 
+    }
+   
 }
